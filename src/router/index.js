e0cbd63be0df2c20db48eafa3b0a372f7c35e628
trinidadroca <troca.ieu2021@student.ie.edu>
2024-10-15 15:49:41 +0200

    path failire HOMEPAGEfix

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Skull from '../components/Skull.vue'
 import AppAccounts from '../components/AppAccounts.vue'
-import Home from '/Users/triniroca/Documents/DEV/ie-bank-fe/src/components/Home.vue';
+import HomePage from '../components/HomePage.vue'
 
 Vue.use(VueRouter)
 
